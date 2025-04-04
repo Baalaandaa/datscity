445e74e4d71c60d272bd27b71481758d5be0d675
balanda <baalaandaa@MacBook-Pro-Kirill-2.local>
2025-04-04 17:48:19 +0300

    libs

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+#define CPPHTTPLIB_OPENSSL_SUPPORT
+
+using namespace std;
+
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cerr << "test" << std::endl;
---
+++ render.cpp
@@ -0,0 +1,3 @@
+#pragma once
+#include "model.cpp"
+using namespace std;
